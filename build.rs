488dd34ac9f13354c61e8267a3d9acedf6c71ddf
Mattis Marjak <mattis.marjak@defendec.com>
2017-03-02 19:47:05 +0200

    initial

---
+++ build.rs
@@ -0,0 +1,34 @@
+extern crate gcc;
+extern crate bindgen;
+
+
+fn main(){
+    println!("cargo:warning={}", bindgen::clang_version().full);
+    let aniplot_bindings = bindgen::Builder::default()
+                                            .no_unstable_rust()
+                                            .enable_cxx_namespaces()
+                                            .header("aniplot/aniplotlib.h")
+                                            .whitelisted_type("ImVec2")
+                                            .whitelisted_type("ImVec4")
+                                            .whitelisted_type("ImVec2d")
+                                            .whitelisted_type("MipBuf_t")
+                                            .whitelisted_type("ImguiTextwrap")
+                                            .whitelisted_type("GraphVisualFlags_")
+                                            .whitelisted_type("PortalRect")
+                                            .whitelisted_type("GraphChannel")
+                                            .whitelisted_type("GraphVisual")
+                                            .whitelisted_type("GraphWidget")
+                                            .opaque_type("std::string")
+                                            .clang_arg("-x")
+                                            .clang_arg("c++")
+                                            .clang_arg("-std=c++11")
+                                            .clang_arg("-Ianiplot/lib/imgui")
+                                            .generate()
+                                            .expect("Failed to generate aniplot bindings");
+    aniplot_bindings.write_to_file("src/generated.rs")
+                    .expect("Could not write aniplot bindings");
+    gcc::Config::new().file("aniplot/aniplotlib.cpp")
+                      .include("aniplot/lib/imgui")
+                      .cpp(true)
+                      .compile("libaniplot.a");
+}
---
+++ examples/imgui_test.rs
@@ -0,0 +1,37 @@
+#[macro_use] extern crate glium;
+#[macro_use] extern crate imgui;
+extern crate imgui_glium_renderer;
+
+mod imgui_support;
+
+use imgui::*;
+use self::imgui_support::Support;
+
+const CLEAR_COLOR: (f32, f32, f32, f32) = (1.0, 1.0, 1.0, 1.0);
+
+fn main() {
+    let mut support = Support::init();
+
+    loop {
+        support.render(CLEAR_COLOR, hello_world);
+        let active = support.update_events();
+        if !active {
+            break;
+        }
+    }
+}
+
+fn hello_world<'a>(ui: &Ui<'a>) {
+    ui.window(im_str!("Hello world"))
+        .size((300.0, 100.0), ImGuiSetCond_FirstUseEver)
+        .build(|| {
+            ui.text(im_str!("Hello world!"));
+            ui.text(im_str!("This...is...imgui-rs!"));
+            if ui.button(im_str!("Tere"), ImVec2::new(100.,50.)) {
+                println!("Tere vajutatud");
+            }
+            ui.separator();
+            let mouse_pos = ui.imgui().mouse_pos();
+            ui.text(im_str!("Mouse Position: ({:.1},{:.1})", mouse_pos.0, mouse_pos.1));
+        })
+}
